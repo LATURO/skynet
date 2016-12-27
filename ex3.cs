5fa09e1d729cb6bfba10a455f129e5a3a0514f94
Andrew Volokh <laturo.volokh@yandex.ru>
2016-12-27 23:27:39 +0400

    Create ex3.cs

---
+++ ex3.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace lllll
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			int a;
+			int s = 0;
+			a = int.Parse(Console.ReadLine());
+	
+			int[,] array = new int[a, a];
+			for (int i = 0; i < a; i++)
+			{
+				for (int j = 0; j < a; j++)
+				{
+					array[i, j] = 1;
+				}
+			}
+			for (int i = 0; i < a; i++)
+			{
+				for (int j = 0; j < i+1; j++)
+				{
+					Console.Write("{0} ", array[i, j]);
+					s += array[i, j];
+				}
+				Console.WriteLine();
+			}
+
+		}
+	}
+}
